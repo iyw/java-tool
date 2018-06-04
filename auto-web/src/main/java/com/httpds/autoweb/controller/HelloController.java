7f061bb7940755ef16916f3daa9afdf182f42185
tanyawen <yawen.tan@yundongpai.net>
2018-06-04 18:31:41 +0800

    fix:add auto-web

---
+++ auto-web/src/main/java/com/httpds/autoweb/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.httpds.autoweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+    @RequestMapping("/get")
+    public Map<String, Object> get() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", 0);
+        map.put("result", "success");
+        return map;
+    }
+}
