faef8bd64da1e582bf843a9d2743c849a984c971
tanyawen <yawen.tan@yundongpai.net>
2018-06-04 18:44:06 +0800

    fix

---
+++ auto-web/src/main/java/com/httpds/autoweb/AutoWebApplication.java
@@ -0,0 +1,12 @@
+package com.httpds.autoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoWebApplication.class, args);
+	}
+}
